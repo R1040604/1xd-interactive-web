912cee395abcbfb4dd3540923eba51086dd6ad64
R1040604 <r1040604@student.thomasmore.be>
2025-02-23 19:51:41 +0100

    4 change css class

--- topics/dom.starter.js
+++ topics/dom.starter.js
@@ -12,7 +12,7 @@ ______ ________  ___
 // select the second paragraph and figure out a way to make the backround color yellow
 // hint: use the style property and .backgroundColor
 // hint: use the querySelector or getElementById function
-document.querySelector("#paragraph2").style.backgroundColor= "yellow";
+document.querySelector("#paragraph2").style.backgroundColor = "yellow";
 // 2 - select the element with id #paragraph3 and change the text to "Never gonna run around and desert you"
 // hint: use the innerHTML property
 document.querySelector("#paragraph3").innerHTML = "Never gonna run around and desert you";
@@ -28,5 +28,5 @@ span.forEach(span => {
 // 4 - change the CSS class
 // select the element with the id "message" and change the css class to "error"
 // hint: use the setAttribute function, or use the className property, or use the classList property
-document.querySelector("#message")
+document.getElementById("message").className = "error";
 // render a ul with li elements
