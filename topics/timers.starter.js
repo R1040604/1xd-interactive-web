68d1d1f41a0bfcbe6c6403ef08947658b55d04dd
R1040604 <r1040604@student.thomasmore.be>
2025-03-02 14:43:01 +0100

    timer functions

--- topics/timers.starter.js
+++ topics/timers.starter.js
@@ -8,9 +8,19 @@ _/  |_|__| _____   ___________  ______
 */
 
 // 1 — start a timer and show a ⏰ in #clock after 10 seconds
-
+setTimeout(() => {
+  console.log("⏰");
+}, "10000");
 // 2 — click the start button to show a ticking clock in #timer that goes up every second
 // clicking stop should clear and stop the timer and reset the clock to 0
+// variable to store our intervalID
+let intervalId;
+
+function keepCounting() {
+  if (!intervalId) {
+    intervalId = setInterval(flashText, 1000);
+  }
+}
 
 // 3 — create an image gallery that shows a new image every 5 seconds and repeats that cycle
 // use the images gallery1.jpg, gallery2.jpg, gallery3.jpg in the images/timers_intervals folder
