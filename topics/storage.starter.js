0cc006d7a9592bf16bb6ef18bebf371867ed325f
R1040604 <r1040604@student.thomasmore.be>
2025-03-17 18:21:51 +0100

    opslag

--- topics/storage.starter.js
+++ topics/storage.starter.js
@@ -8,14 +8,40 @@
 */
 
 // 1 - click on the button, then add your name to localstorage in the key "myName"
-localStorage.getItem('myName', 'Sarah')
-
-// 2 - click on the button to read the value of "myName" from localstorage and display it span#myName
-
-// 3 - click on remove button to remove "myName" from localstorage
-
-// 4 - click on the button to add a movie to the localstorage in the key "movies", show movies in the ul#movieList
-// hint: the value of movies should be an array of strings
-// hint: you can use JSON.stringify to convert an array to string
-// hint: you can use JSON.parse to convert a string to array
-// hint: you can use appendChild to add a new li to the ul#movieList
+document.getElementById('addNameButton').addEventListener('click', function() {
+     localStorage.setItem('myName', 'Sarah');
+ });
+ 
+ // 2 - click on the button to read the value of "myName" from localstorage and display it in span#myName
+ document.getElementById('readNameButton').addEventListener('click', function() {
+     const name = localStorage.getItem('myName');
+     document.getElementById('myName').textContent = name;
+ });
+ 
+ // 3 - click on remove button to remove "myName" from localstorage
+ document.getElementById('removeNameButton').addEventListener('click', function() {
+     localStorage.removeItem('myName');
+ });
+ 
+ // 4 - click on the button to add a movie to the localstorage in the key "movies", show movies in the ul#movieList
+ document.getElementById('addMovieButton').addEventListener('click', function() {
+     const movie = prompt('Enter a movie name:');
+     let movies = JSON.parse(localStorage.getItem('movies')) || [];
+     movies.push(movie);
+     localStorage.setItem('movies', JSON.stringify(movies));
+     displayMovies();
+ });
+ 
+ function displayMovies() {
+     const movies = JSON.parse(localStorage.getItem('movies')) || [];
+     const movieList = document.getElementById('movieList');
+     movieList.innerHTML = '';
+     movies.forEach(function(movie) {
+         const li = document.createElement('li');
+         li.textContent = movie;
+         movieList.appendChild(li);
+     });
+ }
+ 
+ // Initial display of movies
+ displayMovies();
